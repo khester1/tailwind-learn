907cab8045dd335cc7c00ae1abf406f0856f7891
Kyle <kyle.hester@icloud.com>
2024-03-02 19:23:53 -0800

    Start of Template Project. Delete unnecessary files and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import { DatePicker }  from '@fluentui/react';
+import { initializeIcons } from '@fluentui/react/lib/Icons';
+import { Stack } from '@fluentui/react';
+import { PrimaryButton } from '@fluentui/react';
 import './App.css';
+import { TextField } from '@fluentui/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+initializeIcons();
+
+export interface IABCustomDialogProps {
+  //text input
+  text: string | undefined;
+  //date input
+  date: Date | undefined;
 }
 
-export default App;
+const ABCustomDialog: React.FunctionComponent<IABCustomDialogProps> = (props: IABCustomDialogProps) => {
+  const [text, setText] = React.useState<string | undefined>(props.text);
+  const [date, setDate] = React.useState<Date | undefined>(props.date);
+
+  const formatDate = (value?: Date | undefined): string => {
+    if (!value) {
+      return "";
+    }
+
+    let result = ("0" + (value.getMonth() + 1).toString()).slice(-2) + "/";
+    result += ("0" + value.getDate().toString()).slice(-2) + "/";
+    result += value.getFullYear().toString();
+
+    return result;
+  }
+
+  return (
+    <>
+      <TextField
+        label="Label of the Text Input"
+        value={text}
+        onChange={(event: any, newvalue: string | undefined) => { setText(newvalue); }} />
+      <DatePicker
+        label="Label of the Date Input"
+        value={date}
+        onSelectDate={(newValue: Date | undefined | null) => { setDate(newValue ? newValue : undefined) }}
+        formatDate={formatDate}
+      />
+      <div className="footerDiv">
+        <Stack horizontal horizontalAlign={"end"} tokens={{ childrenGap: 10, padding: 10 }}>
+          <PrimaryButton text="OK" onClick={() => {
+            //this code on click of "OK" button returns current state to calling part
+            //@ts-ignore
+            window.returnValue = {
+              text: text,
+              date: date
+            };
+            window.close();
+          }} />
+          <PrimaryButton text="Cancel" onClick={() => {
+            //This code closes the dialog window
+            window.close();
+          }} />
+        </Stack>
+      </div>
+    </>);
+}
+
+export default ABCustomDialog;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let data: any;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (window.location.hostname === "localhost") {
+  data = {
+    text: "Initial String",
+    date: new Date()
+  };
+} else {
+  //following code parses url of dialog to extract required data including "data" parameter
+  const queryString = window.location.search.substring(1);
+  let params: any = {};
+  const queryStringParts = queryString.split("&");
+  for (let i = 0; i < queryStringParts.length; i++) {
+    const pieces = queryStringParts[i].split("=");
+    params[pieces[0].toLowerCase()] = pieces.length === 1 ? null : decodeURIComponent(pieces[1]);
+  }
+
+  //deserializing of the data parameter
+  if (params.data) {
+    data = JSON.parse(params.data);
+  } else {
+    data = {
+      text: "Initial String",
+      date: new Date()
+    };
+  }
+}
+
+//rendering of application and passing parameters inside
+ReactDOM.render(
+  <App text={data.text} date={new Date(data.date)} />,
+  document.getElementById('root')
+);
